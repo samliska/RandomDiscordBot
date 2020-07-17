3ac2b59529d016c12c047b8181cf4265f3cecf7b
samliska <samliska21@gmail.com>
2020-07-17 16:37:23 -0500

    commit players

--- commands/trivia/get_players.js
+++ commands/trivia/get_players.js
@@ -1,2 +1,10 @@
 const Discord = require('discord.js');
 
+module.exports = {
+    name: 'players',
+    description : 'players',
+    needClient : false,
+    execute(message,args){
+        return true;
+    }
+}
